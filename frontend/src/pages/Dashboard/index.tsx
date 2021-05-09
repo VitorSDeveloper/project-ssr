f60acc042914329b380dc5e140db0b70e898e519
Vitor Santos <vitorsdeveloper@gmail.com>
2021-05-09 01:43:25 -0300

    correção em título do BarChart

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,11 +9,11 @@ const Dashboard = () => {
         <>
             <NavBar />
             <div className='container'>
-                <h1 className="text-primary py-3">Dashboard projeto</h1>
+                <h1 className="text-primary py-3">Dashboard de vendas</h1>
 
                 <div className="row px-3">
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secundary">Taxa de sucesso</h5>
+                        <h5 className="text-center text-secundary">Taxa de sucesso (%)</h5>
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
